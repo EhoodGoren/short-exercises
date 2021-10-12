217e00b6c3ddc7b3ecf33b4007e240b00b138753
EhoodGoren <ehoodgoren@gmail.com>
2021-10-12 14:37:38 +0300

    Added some background color changes.
    Changes to set colors, maximum of 3 times. Has 50% chance of failing for each color (will stop future color changes).

--- 03.callback-hell/index.js
+++ 03.callback-hell/index.js
@@ -12,3 +12,48 @@ function product (num1, num2){
 
 const triangle = pythagoras(3,4,5);
 console.log(triangle);
+
+
+console.log("first call");
+setTimeout(()=>{
+    console.log("second call");
+}, 2000);
+console.log("third call");
+// first call > third call > second call
+
+let changesCounter = 0;
+function changeColor(time, color, callback, reject){
+    if(time<1000){
+        time = 1000;
+    }
+    if(changesCounter >= 3){
+        console.log("Reached max attempts");
+        return
+    }
+    setTimeout(() => {
+        const rnd = Math.floor(Math.random() * 10) + 1;
+        const body = document.querySelector("body");
+        if(rnd > 0){
+            changesCounter++;
+            callback()
+            body.style.backgroundColor = color;
+        }
+        else{
+            console.log("your request was rejected ☹")
+            reject()
+        }
+    }, time)
+}
+
+changeColor(1000, "green", () => {
+    changeColor(1000, "red", () => {
+        changeColor(1000, "blue", () => {
+        }, () => {
+            console.log("failed with blue");
+        });
+    }, () => {
+        console.log("failed with red");
+    })
+}, () => {
+    console.log("failed with green");
+});
