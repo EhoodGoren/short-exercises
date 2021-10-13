0e47fd80cfa198d70d5ebd65ba7d95465bec17b8
EhoodGoren <ehoodgoren@gmail.com>
2021-10-14 00:57:41 +0300

    Completed task set 5

--- 04.promise-async/promises-exercises-master/05-callbacks/answers.js
+++ 04.promise-async/promises-exercises-master/05-callbacks/answers.js
@@ -37,7 +37,12 @@ function passwordCheckerCb(email, password, cb){
 function passwordCheckerPrms(email, password){
   return new Promise((resolve, reject) => {
     passwordCheckerCb(email, password, (error, user) => {
-      /* IMPLEMENT ME! */
+      if(user){
+        return resolve(user);
+      }
+      else{
+        return reject(error);
+      }
     });
   });
 }
@@ -57,6 +62,16 @@ function makePromiseFromFunctionWithCallback(fn, ...fnParams){
     - resolves with a value if the callback succeeds
     - rejects with an error if the callback fails
   */
+ return new Promise((resolve, reject) => {
+   fn(...fnParams, (error, user) => {
+    if(user){
+      return resolve(user);
+    }
+    else{
+      return reject(error);
+    }
+   });
+ });
 }
 
 /**
