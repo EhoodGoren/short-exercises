ab6340f0004ef5219ea698b7b9c208b72f523a08
EhoodGoren <ehoodgoren@gmail.com>
2021-10-13 19:24:44 +0300

    Completed task set 01

--- 04.promise-async/promises-exercises-master/01-making-promises/answers.js
+++ 04.promise-async/promises-exercises-master/01-making-promises/answers.js
@@ -6,7 +6,9 @@
  * @returns {Promise<3>}
  */
 function makePromiseResolveWith3(){
-  /* IMPLEMENT ME! */
+  return new Promise((resolve, reject) => {
+    resolve(3);
+  });
 }
 
 /**
@@ -16,7 +18,9 @@ function makePromiseResolveWith3(){
  * @returns {Promise<,"Boo!">}
  */
 function makePromiseRejectWithBoo(){
-  /* IMPLEMENT ME! */
+  return new Promise((resolve, reject) => {
+    reject("Boo!");
+  });
 }
 
 /**
@@ -29,8 +33,12 @@ function makePromiseRejectWithBoo(){
 
 function makePromiseWithConstructor(itShouldResolve){
   return new Promise((resolve, reject) => {
-    /* If itShouldResolve is true, call resolve */
-    /* If itShouldResolve is false, call reject */
+    if(itShouldResolve){
+      resolve();
+    }
+    else{
+      reject();
+    }
   });
 }
 
@@ -43,7 +51,11 @@ function makePromiseWithConstructor(itShouldResolve){
  * @return {Promise<any>} - A promise that will resolve with the value after delayInMs milliseconds
  */
 function makeDelayPromise(value, delayInMs){
-  /* Return a promise that resolves with the value after delayInMs */
+  return new Promise((resolve,reject) => {
+    setTimeout(() => {
+      resolve(value);
+    }, delayInMs);
+  })
 }
 
 module.exports = {
