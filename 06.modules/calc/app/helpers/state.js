45ce49b1ef1a098b55d179ce950acc0db064b956
EhoodGoren <ehoodgoren@gmail.com>
2021-10-17 22:31:40 +0300

    Displays clicked number in input area

---
+++ 06.modules/calc/app/helpers/state.js
@@ -0,0 +1,5 @@
+const state = {
+    firstNumber: "",
+    secondNumber: "",
+    callBack: ""
+}
--- 06.modules/calc/app/main.js
+++ 06.modules/calc/app/main.js
@@ -1,1 +1,14 @@
-import { style } from "./styles.css";
+import style from "./styles.css";
+import * as math from "./helpers/math";
+
+const calcContainer = document.querySelector(".container");
+calcContainer.addEventListener("click", calcClick);
+
+function calcClick(event){
+    if(! event.target.type === "button") return;
+    if(!isNaN(event.target.value)){
+        const num = event.target.value;
+        const numField = document.querySelector(".result");
+        numField.value = numField.value + num;
+    }
+}
