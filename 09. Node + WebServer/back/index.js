58a5057f36cc34d0d5a5c352a54252a6b015ece4
EhoodGoren <ehoodgoren@gmail.com>
2021-10-21 01:07:08 +0300

    Sending the information will return a response from the server if you got accepted or not.

---
+++ 09. Node + WebServer/back/index.js
@@ -0,0 +1,81 @@
+const http = require("http");
+const fs = require("fs");
+const db = require("./db");
+
+const server = http.createServer((req, res) => {
+    const headers = {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": "true",
+        "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT",
+        "Access-Control-Allow-Headers": "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers",
+        // "Acces-Control-Allow-Headers": "*",
+        "Content-Type":"text",
+    };
+    let goodData = false
+    req.on("data", (data) => {
+        goodData = checkData(data);
+    });
+    req.on("end", ()=>{
+        if (goodData === "good"){
+            // res.writeHead(200, "ok", {"Content-Type":"text"});
+            res.writeHead(200, "ok", headers);
+            res.write("welcome to Cyber4s!");
+            res.end();
+            return
+        }
+        else {
+            // res.writeHead(200, "ok", {"Content-Type":"text"})
+            res.writeHead(200, "ok", headers)
+            res.write("Sorry you got declined");
+            res.end();
+            return
+        }
+    });
+});
+
+const port = 9000;
+server.listen(port, () => {
+    console.log("listening");
+});
+
+function checkData(data){
+    const readAbleData = JSON.parse(data.toString());
+    const { name, age, ability } = readAbleData;
+    const goodName = checkName(name);
+    const goodAge = checkAge(age);
+    const goodAbility = checkAbility(ability);
+    if(goodName && goodAge && goodAbility){
+        return "good";
+    } else {
+        return "not good";
+    }
+}
+
+function checkName(name){
+    const badNames = db.nameNotEqual;
+    for(let badName of badNames){
+        if(badName === name){
+            return false;
+        }
+    }
+    return true;
+}
+
+function checkAge(age){
+    const minAge = db.minAge;
+    const maxAge = db.maxAge;
+    if(age <= maxAge && age >= minAge){
+        return true;
+    }
+    return false;
+}
+
+function checkAbility(abiltiy){
+    const goodAbilities = db.ability;
+    for(let goodAbility of goodAbilities){
+        if(abiltiy === goodAbility){
+            return true;
+        }
+    }
+    return false;
+}
--- 09. Node + WebServer/front/index.js
+++ 09. Node + WebServer/front/index.js
@@ -2,8 +2,10 @@ const submitBtn = document.querySelector("#submit");
 submitBtn.addEventListener("click", handleSubmit);
 
 async function handleSubmit(event){
+    event.preventDefault();
     if(!checkInputs()) return
-    const response = await axios.post();
+    const inputs = getInputs();
+    const response = await axios.post("http://localhost:9000", inputs);
 }
 
 function checkInputs(){
@@ -11,10 +13,19 @@ function checkInputs(){
     // Checks if one of the inputs if empty
     for(let input of formInputs){
         if(input.value === ""){
-            alert(`Please fill ${input}!`);
+            alert(`Please fill ${input.name}!`);
             return false
         }
     }
     return true;
+}
+
+function getInputs(){
+    const inputs = document.querySelectorAll(".form_inputs");
+    return {
+        name: inputs[0].value,
+        age: inputs[1].value,
+        ability: inputs[2].value,
+    }
 }
 
